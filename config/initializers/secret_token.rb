1d5de23052c74818cb673fb40e37a057c8301c47
liuben10 <benjamindliu@yahoo.com>
2013-10-18 02:00:13 -0700

    initial commit rails skeleton autogenerated code

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scheduler::Application.config.secret_token = 'd17a6510c0373b4296efa727fb5816768700e7e19c01387087443a6f70082fcaae3999ab2cd6d2464a63ae5b025878cf3922ea59f3d0b7a1295bd4604d596a99'
