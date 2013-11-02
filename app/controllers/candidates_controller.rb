8b49a8a18a5f053beede97e0f57ac6b74e9ed94c
liuben10 <benjamindliu@yahoo.com>
2013-11-02 16:47:58 -0700

    simplified some of the code

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -4,23 +4,16 @@ class CandidatesController < ApplicationController
   def create
     name = params[:candidate][:name]
     email = params[:candidate][:email]
-    debugmsg = "Malformed input: "
-    flag = false
-    if email.nil? or email.strip.empty?
-      debugmsg += "Email is empty"
-      flag = true
-    end
-    if name.nil? or name.strip.empty?
-      debugmsg += " Name is empty"
-      flag = true
-    end
-    if flag
+    debugmsg = "Malformed input, email or name was empty"
+    if email.nil? or email.strip.empty? or name.nil? or name.strip.empty?
       flash[:notice] = debugmsg
+      redirect_to welcome_index_path
     else
       Candidate.create! params[:candidate]
       flash[:notice] = "New Candidate created with name: #{name} and email: #{email}"
+      redirect_to candidate_path(params[:candidate][:name])
     end
-     redirect_to candidate_path(params[:candidate][:name])
+
   end
 
   def show
