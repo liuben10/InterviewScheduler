e51fabda0288d6cc69f08766773e57ec71583cfa
liuben10 <benjamindliu@yahoo.com>
2013-11-06 23:06:43 -0800

    removed bad password addin

--- db/migrate/20131104021202_add_password_to_recruiters.rb
+++
@@ -1,6 +0,0 @@
-class AddPasswordAndCompanyToRecruiters < ActiveRecord::Migration
-  def change
-    add_column :recruiters, :password, :string
-    add_column :recruiters, :company, :string
-  end
-end
