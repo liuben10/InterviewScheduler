b9a36dba96dcec23ca3d672b3b6f6f3e9eed2e63
saasbook <saasbook@saasbook.(none)>
2013-11-07 16:10:55 -0800

    calendar stuff but bonghyun told me to pull2~2~

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,13 @@
 //= require_tree .
 
 $(document).foundation();
+
+$(document).ready(function() {
+
+    // page is now ready, initialize the calendar...
+
+    $('#calendar').fullCalendar({
+        // put your options and callbacks here
+    })
+
+});
