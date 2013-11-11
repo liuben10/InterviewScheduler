ef976dc6a581873f95a7c99779a4132be9910855
radheshdevendran <radheshdevendran@berkeley.edu>
2013-11-10 23:10:41 -0800

    More refactoring of WelcomeController#show

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,9 +2,8 @@ class WelcomeController < ApplicationController
   def index
     @candidate = Candidate.new
     if not session[:authenticated_user].nil?
-      sessname = session[:authenticated_user]
-      candidate = Candidate.find_by_name(sessname)
-      recruiter = Recruiter.find_by_name(sessname)
+      candidate = Candidate.find_by_name(session[:authenticated_user])
+      recruiter = Recruiter.find_by_name(session[:authenticated_user])
       if not candidate.nil?
         redirect_to candidate_path(candidate.name)
       elsif not recruiter.nil?
@@ -15,19 +14,17 @@ class WelcomeController < ApplicationController
 
   def show
     Rails.logger.debug params[:userid]
-    foundRecruiter = Recruiter.find_by_email(params[:userid])
-    foundCandidate = Candidate.find_by_email(params[:userid])
-    if not foundRecruiter.nil?
-      foundUser = foundRecruiter
-      #redirect_to recruiter_show_path
-      redirectPath = recruiter_path(foundRecruiter.name)
-    elsif not foundCandidate.nil?
-      foundUser = foundCandidate
-      #redirect_to candidate_show_path
-      redirectPath = candidate_path(foundCandidate.name)
+    foundUser = Recruiter.find_by_email(params[:userid])
+    if not foundUser.nil?
+      redirectPath = recruiter_path(foundUser.name)
     else
-      flash[:notice] = "Email not found"
-      redirect_to welcome_index_path and return
+      foundUser = Candidate.find_by_email(params[:userid])
+      if not foundUser.nil?
+        redirectPath = candidate_path(foundUser.name)
+      else
+        flash[:notice] = "Email not found"
+        redirect_to welcome_index_path and return
+      end
     end
     if foundUser.password != params[:passid]
       flash[:notice] = "Password was incorrect, please try again"
