70dce65e81b1ccc7b6e49c50f89a3b2e7b420f57
radheshdevendran <radheshdevendran@berkeley.edu>
2013-12-09 17:03:00 -0800

    Removed duplication of fields in messages

--- db/migrate/20131207221551_add_name_to_messages.rb
+++
@@ -1,8 +0,0 @@
-class AddNameToMessages < ActiveRecord::Migration
-  def change
-    add_column :messages, :title, :string
-    add_column :messages, :from, :string
-    add_column :messages, :to, :string
-    add_column :messages, :content, :text
-  end
-end
